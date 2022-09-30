af62b02a2d0c64fa96522cd0a5558447e8ada2cd
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-09-30 15:28:45 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 (() => {
 	const items = document.querySelectorAll(".item");
-	console.log(`Number of categories: ${items.length}`);
+	console.log(`Number of categories: ${items.length}\n`);
 	items.forEach(el => {
 		console.log(`Category:  ${el.children[0].textContent}`);
-		console.log(`Elements: ${el.children[1].children.length}`);
+		console.log(`Elements: ${el.children[1].children.length}\n`);
 	})
 })();
